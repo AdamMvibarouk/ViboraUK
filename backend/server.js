9e017aafd422d36da6cbd56c596c0a27b2b4e29f
f4yette <ahmedahmedd1012@outlook.com>
2025-11-29 08:13:22 +0000

    .env and server.js pushed to ahmed dev
    updated server.js and created an .env file

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+// server.js
+
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const productsRoute = require("./routes/products");
+const cartRoute = require("./routes/cart");
+const ordersRoute = require("./routes/orders");
+const usersRoute = require("./routes/users");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// API routes
+
+app.use("api/products", productsRoute);
+app.use("api/cart", cartRoute);
+app.use("api/orders", ordersRoute);
+app.use("api/users", usersRoute);
+
+const PORT = process.env.PORT || 5000;
+
+App.listen(PORT, () => {
+  console.log(`Backend running on port ${PORT}`);
+});
