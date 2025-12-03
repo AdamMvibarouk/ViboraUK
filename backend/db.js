97cdba6b5395d213a68c26b09f2ee09b9eb43e12
ahm3ddd-1 <ahmedahmedd1012@outlook.com>
2025-12-03 22:37:24 +0000

    Update db.js
    updated db

--- backend/db.js
+++ backend/db.js
@@ -4,8 +4,8 @@ require("dotenv").config();
 const db = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
 
-module.exports = { db };
+module.exports = db;
