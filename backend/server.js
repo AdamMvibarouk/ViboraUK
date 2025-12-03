458e9f636f97e8dee40c82fac9a24b27b9e21b0b
ahm3ddd-1 <ahmedahmedd1012@outlook.com>
2025-12-03 15:29:30 +0000

    removed as it was just a test
    testing the pages to see if they would load, no longer need them in my branch.

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,29 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const dotenv = require("dotenv");
 
-// routes
+dotenv.config();
+
+const authRoutes = require("./routes/auth");
+const ordersRoutes = require("./routes/orders");
 const productsRoute = require("./routes/products");
 const cartRoute = require("./routes/cart");
-const ordersRoute = require("./routes/orders");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// api routes
+app.use("/api/auth", authRoutes);
+app.use("/api/orders", ordersRoutes);
 app.use("/api/products", productsRoute);
 app.use("/api/cart", cartRoute);
-app.use("/api/orders", ordersRoute);
+
+app.get("/", (req, res) => {
+  res.send("Backend is running ✅");
+});
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log(`Backend running on port ${PORT}`);
 });
