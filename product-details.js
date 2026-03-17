743f5cf68b77a9cfdd58dd917fb040718349b79a
Joseba6Bilbao <josebabilbaoe@gmail.com>
2026-03-17 16:35:48 +0000

    UPDATED PRODUCT PAGE TO SHOW IMAGES FOR PRODUCTS OF ALL CATEGORIES

--- product-details.js
+++ product-details.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", async () => {
+ddocument.addEventListener("DOMContentLoaded", async () => {
   const params = new URLSearchParams(window.location.search);
   const productId = params.get("id");
   if (!productId) return;
@@ -46,7 +46,77 @@ document.addEventListener("DOMContentLoaded", async () => {
     "Head Speed Motion 2025":
       "database/images/products/rackets/HEAD EVO EXTREME 2025.jpg",
     "Babolat Air Origin":
-      "database/images/products/rackets/Babolat-Air-Vertuo-Padel-Racket-2025.png"
+      "database/images/products/rackets/Babolat-Air-Vertuo-Padel-Racket-2025.png",
+    "Short Sleeve Training Top Womens Red":
+      "database/images/products/tshirts/Short Sleeve Training Top Womens Red.jpg",
+    "Short Sleeve Training Top Mens Red":
+      "database/images/products/tshirts/Short Sleeve Training Top Mens Red.jpg",
+    "Short Sleeve Training Top Mens Navy":
+      "database/images/products/tshirts/Short Sleeve Training Top Mens Navy.jpg",
+    "Short Sleeve Training Top Womens Navy":
+      "database/images/products/tshirts/Short Sleeve Training Top Womens Navy.jpg",
+      "Short Sleeve Training Top Mens Black":
+        "database/images/products/tshirts/Short Sleeve Training Top Mens Black.jpg",
+      "Short Sleeve Training Top Mens White":
+        "database/images/products/tshirts/Short Sleeve Training Top Mens White.jpg",
+      "T-Shirt Bullpadel Paquito 25I White":
+        "database/images/products/tshirts/T-SHIRT BULLPADEL PAQUITO 25I WHITE.jpg",
+      "T-Shirt Bullpadel Chingotto 25I Stone":
+        "database/images/products/tshirts/T-SHIRT BULLPADEL PAQUITO 25I WHITE.jpg",
+      "T-Shirt Bullpadel Di Nenno 25I Hybiscus":
+        "database/images/products/tshirts/T-SHIRT BULLPADEL PAQUITO 25I WHITE.jpg",
+      "T-Shirt Bullpadel Tello 25I Blue Green":
+        "database/images/products/tshirts/T-SHIRT BULLPADEL PAQUITO 25I WHITE.jpg",
+      "T-Shirt Bullpadel Batea Woman":
+        "database/images/products/tshirts/T-SHIRT BULLPADEL BATEA WOMAN.jpg",
+      "T-Shirt Bullpadel Gemma 25V Woman":
+        "database/images/products/tshirts/t-shirt-bullpadel-gemma-25v-water-green.jpg",
+      "T-Shirt Nox Pro 2025":
+        "database/images/products/tshirts/T-SHIRT NOX PRO 2025.jpg",
+        "T-Shirt Lacoste TH5195":
+          "database/images/products/tshirts/T-SHIRT LACOSTE TH5195.jpg",
+      "Padel Core Carry Case":
+      "database/images/products/bags/Padel Core Carry Case.JPG",
+    "Bullpadel BPP26013 Hack Bag":
+      "database/images/products/bags/Bullpadel BPP26013 Hack Bag.jpg",
+    "Bullpadel BPP26021 Pearl Bag":
+      "database/images/products/bags/Bullpadel BPP26021 Pearl Bag.jpg",
+    "Bullpadel BPP25022 Xplo Red Bag":
+      "database/images/products/bags/Bullpadel BPP25022 Xplo Red Bag.jpg",
+    "Bullpadel BPP25015 Tour Bag":
+      "database/images/products/bags/Bullpadel BPP25015 Tour Bag.jpg",
+    "Varlion Summum Lime Green Bag":
+      "database/images/products/bags/lime-green-varlion-summum-padel-bag-3338933439.jpg",
+    "Bullpadel BPP26013 Hack Racket Bag Grey-Green":
+      "database/images/products/bags/PALETERO BULLPADEL BPP26013 HACK GRIS VERDOSO.jpg",
+    "Babolat Court Padel X3 Balls Canister":
+      "database/images/products/balls/BABOLAT COURT PADEL X3 BALLS CANISTER.jpg",
+    "Wilson Premier Padel Balls Canister":
+      "database/images/products/balls/WILSON PREMIER PADEL BALLS CANISTER.jpg",
+    "3-Ball Can Nox Nerbo":
+      "database/images/products/balls/3-BALL CAN NOX NERBO.jpg",
+    "Bullpadel Premium Pro Boat":
+      "database/images/products/balls/BULLPADEL PREMIUM PRO BOAT.jpg",
+    "Bullpadel Train Ball Jar 465464":
+      "database/images/products/balls/BULLPADEL TRAIN BALL JAR 465464.jpg",
+    "Bullpadel Neuron Vibram 25V Clay":
+      "database/images/products/shoes/TRAINERS BULLPADEL NEURON VIBRAM 25V CLAY.jpg",
+    "Bullpadel Hybrid Fly 25I Steel Blue":
+      "database/images/products/shoes/trainers-bullpadel-hybrid-fly-25i-steel-blue-154344606.jpg",
+    "Bullpadel Vertex Vibram 25V Yellow":
+      "database/images/products/shoes/trainers-bullpadel-vertex-vibram-25v-yellow-1522073331.jpg",
+    "Bullpadel Premier P1 White":
+      "database/images/products/shoes/trainers-bullpadel-premier-p1-white-687461239.jpg",
+    "Babolat Jet Mach 3 All Court Blue/Orange Men 30523629":
+      "database/images/products/shoes/BABOLAT JET MACH 3 ALL COURT BLUE ORANGE MEN.jpg",
+    "K-Swiss Hypercourt Supreme 2 White 09071102":
+      "database/images/products/shoes/KSWISS HYPERCOURT SUPREME 2 WHITE 09071102.jpg",
+    "Joma Master 1000 Men 25 Clay Fluorescent Yellow TM100S2599CC":
+      "database/images/products/shoes/joma-master-1000-men-2509-yellow-2025-padel-shoes-975772017.jpg",
+    "Bullpadel Trainers Buker JR 25I White":
+      "",
+    "Wilson Rush Pro 4.5 White/Blue":
+      "database/images/products/shoes/WILSON RUSH PRO 4.5 WHITE BLUE.jpg"
   };
 
 const sportswearImages = {
